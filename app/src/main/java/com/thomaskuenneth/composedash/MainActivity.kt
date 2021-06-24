ae3d4c798cc3fba5f28d3a97fc965d88974d69e3
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-06-24 17:07:05 +0200

    Added variables for lives and gems collected; also, level ends when all gems are collected

--- app/src/main/java/com/thomaskuenneth/composedash/MainActivity.kt
+++ app/src/main/java/com/thomaskuenneth/composedash/MainActivity.kt
@@ -6,18 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import com.thomaskuenneth.composedash.ui.theme.ComposeDashTheme
 import kotlinx.coroutines.delay
@@ -43,6 +46,19 @@ val level = """
     ########################################
     """.trimIndent()
 
+const val PLAYER = 0x1F3C3
+
+const val GEM = 0x1F48E
+const val CHAR_GEM = 'X'
+
+const val BRICK = 0x1F9F1
+
+const val ROCK = 0x1FAA8
+
+const val SAND = 0x2591
+
+const val NUMBER_OF_LIVES = 3
+
 class MainActivity : ComponentActivity() {
 
     @ExperimentalStdlibApi
@@ -50,12 +66,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val levelData = remember {
-                createLevelDate()
-            }
             ComposeDashTheme {
                 Surface(color = Color.Black) {
-                    ComposeDash(levelData)
+                    ComposeDash()
                 }
             }
         }
@@ -64,33 +77,66 @@ class MainActivity : ComponentActivity() {
     @ExperimentalStdlibApi
     @ExperimentalFoundationApi
     @Composable
-    fun ComposeDash(levelData: SnapshotStateList<Char>) {
-        LazyVerticalGrid(
-            modifier = Modifier.fillMaxSize(),
-            cells = GridCells.Fixed(COLUMNS)
-        ) {
-            itemsIndexed(levelData, itemContent = { index, item ->
-                var background = Color.Transparent
-                val symbol = when (item) {
-                    '#' -> 0x1F9F1
-                    'X' -> 0x1F48E
-                    'O' -> 0x1FAA8
-                    '.' -> {
-                        background = Color(0xffc2b280)
-                        0x2591
+    fun ComposeDash() {
+        val key = remember { mutableStateOf(0L) }
+        val levelData = remember(key.value) {
+            createLevelDate()
+        }
+        val gemsTotal = 1 // remember(key.value) { Collections.frequency(levelData, CHAR_GEM) }
+        val lives = remember(key.value) { mutableStateOf(NUMBER_OF_LIVES) }
+        val gemsCollected = remember(key.value) { mutableStateOf(0) }
+        Box {
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxSize(),
+                cells = GridCells.Fixed(COLUMNS)
+            ) {
+                itemsIndexed(levelData, itemContent = { index, item ->
+                    var background = Color.Transparent
+                    val symbol = when (item) {
+                        '#' -> BRICK
+                        CHAR_GEM -> GEM
+                        'O' -> ROCK
+                        '.' -> {
+                            background = Color(0xffc2b280)
+                            SAND
+                        }
+                        '@' -> PLAYER
+                        else -> 32
                     }
-                    '@' -> 0x1F3C3
-                    else -> 32
-                }
-                Text(
+                    Text(
+                        modifier = Modifier
+                            .background(background)
+                            .clickable {
+                                movePlayerTo(levelData, index, gemsCollected)
+                            },
+                        text = symbol.unicodeToString()
+                    )
+                })
+            }
+            Text(
+                text = "${PLAYER.unicodeToString()}${lives.value} ${GEM.unicodeToString()}${gemsCollected.value}",
+                color = Color.White,
+                style = TextStyle(fontSize = 16.sp),
+                modifier = Modifier.align(alignment = Alignment.BottomCenter)
+            )
+            if (gemsTotal == gemsCollected.value) {
+                Box(
                     modifier = Modifier
-                        .background(background)
+                        .fillMaxSize()
+                        .background(color = Color(0xa0000000))
                         .clickable {
-                            movePlayerTo(levelData, index)
-                        },
-                    text = String(Character.toChars(symbol))
-                )
-            })
+                            key.value += 1
+                        }
+                ) {
+                    Text(
+                        "Well done!\nTap to restart",
+                        style = TextStyle(fontSize = 48.sp, textAlign = TextAlign.Center),
+                        color = Color.White,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                    )
+                }
+            }
         }
     }
 
@@ -106,7 +152,11 @@ class MainActivity : ComponentActivity() {
         return data
     }
 
-    private fun movePlayerTo(levelData: SnapshotStateList<Char>, desti: Int) {
+    private fun movePlayerTo(
+        levelData: SnapshotStateList<Char>,
+        desti: Int,
+        gemsCollected: MutableState<Int>
+    ) {
         val start = levelData.indexOf('@')
         if (start == desti) return
         val startX = start % COLUMNS
@@ -120,11 +170,11 @@ class MainActivity : ComponentActivity() {
             var x = startX
             var y = startY
             while (current != -1 && y != destiY) {
-                current = walk(levelData, current, x, y)
+                current = walk(levelData, current, x, y, gemsCollected)
                 y += dirY
             }
             while (current != -1 && current != desti) {
-                current = walk(levelData, current, x, y)
+                current = walk(levelData, current, x, y, gemsCollected)
                 x += dirX
             }
         }
@@ -134,12 +184,13 @@ class MainActivity : ComponentActivity() {
         levelData: SnapshotStateList<Char>,
         current: Int,
         x: Int,
-        y: Int
+        y: Int,
+        gemsCollected: MutableState<Int>
     ): Int {
         val newPos = (y * COLUMNS) + x
         when (levelData[newPos]) {
             'X' -> {
-                // TODO: update gem count
+                gemsCollected.value += 1
             }
             'O', '#' -> {
                 return -1
@@ -184,3 +235,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+fun Int.unicodeToString(): String = String(Character.toChars(this))
